b4fa0683c1b7781253b08c570a5272ba0fed4004
Prithivee Ramalingam <prithivee.r@tigeranalytics.com>
2024-06-03 11:49:40 +0530

    Initial commit

--- src/house_price_prediction/training.py
+++ src/house_price_prediction/training.py
@@ -18,6 +18,16 @@ def income_cat_proportions(data):
     return data["income_cat"].value_counts() / len(data)
 
 
+def stratifiedShuffleSplit(housing):
+    train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+
+    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+    for train_index, test_index in split.split(housing, housing["income_cat"]):
+        strat_train_set = housing.loc[train_index]
+        strat_test_set = housing.loc[test_index]
+    return train_set, test_set, strat_train_set, strat_test_set
+
+
 def perform_stratified_sampling(housing):
     train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
 
---
+++ src/scripts/ingest_data.py
@@ -0,0 +1,68 @@
+import argparse
+import os
+import tarfile
+
+import numpy as np
+import pandas as pd
+from six.moves import urllib
+
+from house_price_prediction import data_ingestion, training
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+
+def ingest_input_data(output_folder):
+    """Ingest the input data"""
+    raw_data_path = output_folder + '/raw'
+    os.makedirs(raw_data_path, exist_ok=True)
+    DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+    HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+    data_ingestion.fetch_housing_data(HOUSING_URL, raw_data_path)
+    # Load the data
+    housing = data_ingestion.load_housing_data(raw_data_path)
+    housing["income_cat"] = pd.cut(
+        housing["median_income"],
+        bins=[0.0, 1.5, 3.0, 4.5, 6.0, np.inf],
+        labels=[1, 2, 3, 4, 5],
+    )
+    print("Data Loaded Successfully")
+    # Train and Test split the data
+    train_set, test_set, train, test = training.stratifiedShuffleSplit(housing)
+    # Pre process the data
+    housing, y_train, X_train = data_ingestion.impute_data(train)
+    X_train = training.get_household_related_information(X_train)
+
+    X_cat = train[["ocean_proximity"]]
+    X_train = X_train.join(pd.get_dummies(X_cat, drop_first=True))
+
+    housing, y_test, X_test = data_ingestion.impute_data(test)
+    X_test = training.get_household_related_information(X_test)
+
+    X_cat = housing[["ocean_proximity"]]
+    X_test = X_test.join(pd.get_dummies(X_cat, drop_first=True))
+
+    # Save the output to the folder
+    processed_data_path = os.path.join(output_folder, 'processed')
+    x_train_csv_path = os.path.join(processed_data_path, 'X_train.csv')
+    y_train_csv_path = os.path.join(processed_data_path, 'y_train.csv')
+    x_test_csv_path = os.path.join(processed_data_path, 'X_test.csv')
+    y_test_csv_path = os.path.join(processed_data_path, 'y_test.csv')
+
+    os.makedirs(processed_data_path, exist_ok=True)
+    X_train = X_train.to_csv(x_train_csv_path, index=False)
+    y_train = y_train.to_csv(y_train_csv_path, index=False)
+    X_test = X_test.to_csv(x_test_csv_path, index=False)
+    y_test = y_test.to_csv(y_test_csv_path, index=False)
+    print("Train Test dataset split Successfully and saved")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("output_folder", help="Output Folder path")
+    args = parser.parse_args()
+    ingest_input_data(args.output_folder)
+
+
+if __name__ == '__main__':
+    main()
